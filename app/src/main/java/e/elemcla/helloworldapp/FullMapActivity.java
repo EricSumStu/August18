439a628d6c7a2c89415500978a1c6a7b5927f12d
eahmhod <hoda.ahmed@ericsson.com>
2018-08-13 14:04:59 +0100

    Completed screens 1-4 screen 1 - splash screen screen 2 - menu with options "Full Map" & "Desks" screen 3 - full map of ericsson with zoom functions screen 4 - screens that shows dropdowns for selecting zones (from..to) and go button

---
+++ app/src/main/java/e/elemcla/helloworldapp/FullMapActivity.java
@@ -0,0 +1,24 @@
+package e.elemcla.helloworldapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class FullMapActivity extends AppCompatActivity{
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_full_map);
+
+            ImageView imageView = (ImageView) findViewById(R.id.zoomImageView);
+
+            Glide
+                    .with(this)
+                    .load(R.drawable.anothermap)
+                    .into(imageView);
+        }
+    }
