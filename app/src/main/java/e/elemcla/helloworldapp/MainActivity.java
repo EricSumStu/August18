32560c618e842cd50b86ebb224a255ac9fd38c9c
eahmhod <hoda.ahmed@ericsson.com>
2018-08-13 10:31:29 +0100

    Merge branch 'master' of https://github.com/EricSumStu/August18
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/e/elemcla/helloworldapp/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	build.gradle

--- app/src/main/java/e/elemcla/helloworldapp/MainActivity.java
+++ app/src/main/java/e/elemcla/helloworldapp/MainActivity.java
@@ -1,14 +1,9 @@
 package e.elemcla.helloworldapp;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
@@ -16,26 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps_view);
-
-        ImageButton button = findViewById(R.id.imageButton5);
-
-        button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NewPage.class);
-                startActivity(intent);
-            }
-        });
-
-        }
-    }
-
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent homeIntent = new Intent(MainActivity.this, MapsViewActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
--- app/src/main/java/e/elemcla/helloworldapp/MapsPage.java
+++ app/src/main/java/e/elemcla/helloworldapp/MapsPage.java
@@ -2,21 +2,19 @@ package e.elemcla.helloworldapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 
-public class NewPage extends AppCompatActivity{
+public class MapsPage extends AppCompatActivity{
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_newpage);
+            setContentView(R.layout.activity_maps_page);
 
             ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            Button button = (Button) findViewById(R.id.button);
 
             Glide
                     .with(this)
