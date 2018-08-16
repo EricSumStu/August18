0862706db500be72be565508f344237a47636782
eahmhod <hoda.ahmed@ericsson.com>
2018-08-16 14:47:08 +0100

    most of screen 6 - the correct maps (coloured) are shown when two zones are selected and the "go" button is clicked

---
+++ app/src/main/java/e/elemcla/helloworldapp/GroundFloorRouteActivity.java
@@ -0,0 +1,30 @@
+package e.elemcla.helloworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class GroundFloorRouteActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ground_floor_route);
+
+//        int numOfPins = 14;
+//        ImageView[] pins = new ImageView[numOfPins - 1];
+//
+//        for (int index = 0; index <= numOfPins - 1; index ++){
+//            pins[index] = (ImageView)findViewById(R.id.Zone_1);
+//        }
+//
+//        for(ImageView pin : pins){
+//            if (secondOption.equals(pin.getId())||firstOption.equals(pin.getId())  ){
+//                pin.setVisibility(View.VISIBLE);
+//            }
+//        }
+
+    }
+}
